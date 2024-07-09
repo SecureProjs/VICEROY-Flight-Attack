5e42caad8cfb0f64a15a6c772e8fa888f8937b80
EmoryClaire <emoryclaire07@gmail.com>
2024-07-09 09:43:41 -0500

    Update Main.java
    Added my code to the procedure I added. - Emory W

--- src/application/Main.java
+++ src/application/Main.java
@@ -60,7 +60,18 @@ public class Main extends Application {
         //Scene can take in a "pane" which is basically any node type
         Scene debugWindow = new Scene(HelloText, 600, 500);
         controllerStage.setScene(debugWindow);
-
+	 //Changes the coordinates of the scene
+        controllerStage.setX(60);
+        controllerStage.setY(100);
+        //Takes away the title bar of the scene
+        controllerStage.initStyle(StageStyle.UNDECORATED);
+        //Creates a new scene
+        Scene ColoredWindow = new Scene(Root, 600, 500);
+        //Sets the background color of the new scene to black
+        Root.setStyle("-fx-background-color: black;");
+        //Adds scene to stage
+        controllerStage.setScene(ColoredWindow);
+        controllerStage.show();
         return controllerStage;
     }
 
